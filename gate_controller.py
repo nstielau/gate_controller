51e5254626bb2bb9d66a97e1718f66e0ccd3aa88
Nick Stielau <nick.stielau@gmail.com>
2022-09-05 16:22:26 -0700

    Ordering callback definition

--- gate_controller.py
+++ gate_controller.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import time
 import sys
 
@@ -20,10 +21,10 @@ GPIO.setmode(GPIO.BOARD) # Use Board numbers https://pinout.xyz/
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setwarnings(False)
 
-GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=button_callback) # Setup event on pin rising edge
-
 def button_callback(channel):
     logger.info("Button was pushed!")
     GPIO.output(LED_PIN, True)
     time.sleep(0.2)    
     GPIO.output(LED_PIN, False)
+
+GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=button_callback) # Setup event on pin rising edge
